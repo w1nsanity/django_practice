b8ef8a06e381569fe5b37618b5c83868689bf9f3
artur <stephencurry30@inbox.ru>
2023-11-25 14:58:40 +0300

    light recommendation system by tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -51,10 +52,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # Формирование списка похожих статей.
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'new_comment': new_comment,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # получение статьи по идентификатору
